5d8295ea457069778c174bdca5704e9fe07aee51
Gabriel Poret <gporet@consoneo.com>
2014-12-26 20:50:27 +0100

    sensio insight clean code

--- Entity/Annuaire.php
+++ Entity/Annuaire.php
@@ -3,7 +3,6 @@
 namespace Consoneo\Bundle\EcoffreFortBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
